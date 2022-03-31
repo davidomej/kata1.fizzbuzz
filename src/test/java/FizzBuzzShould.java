a6618d2f5135d7651d5487a06f7e7832ec019ab8
david <davidomej@hotmail.com>
2022-03-31 20:34:53 +0100

    Test Correcto y con fallo

--- src/test/java/FizzBuzzShould.java
+++ src/test/java/FizzBuzzShould.java
@@ -13,11 +13,15 @@ public class FizzBuzzShould {
         assertEquals("FizzBuzz", fb.play(15));
     }
 
-    /*
+
     @Test
     void test_failed(){
+        FizzBuzz fb = new FizzBuzz();
         assertEquals("3",fb.play(3));
+        assertEquals("Fizz", fb.play(1));
+        assertEquals("Buzz", fb.play(3));
+        assertEquals("FizzBuzz", fb.play(15));
     }
 
-    */
+
 }
