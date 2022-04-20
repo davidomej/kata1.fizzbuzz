16b306e79bc6b81c161437c5bb3e928d4814cbae
david <davidomej@hotmail.com>
2022-04-20 22:45:23 +0100

    All Test Green

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,9 +1,13 @@
 public class FizzBuzz {
 
     public String play(int i) {
-       if(i%3==0 && i%5==0){
-           return "FizzBuzz";
-       }
-       return ""+i;
+        if(i%5==0 && i%3==0){
+            return "FizzBuzz";
+        }else if(i%3==0){
+            return "Fizz";
+        }else if(i%5==0){
+            return "Buzz";
+        }
+        return String.valueOf(i);
     }
 }
--- src/test/java/FizzBuzzShould.java
+++ src/test/java/FizzBuzzShould.java
@@ -4,8 +4,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FizzBuzzShould {
 
+
+    //Numbers not multiples of 3 or 5 return the same number
+    //Numbers multiples of 3 return Fizz
+    //Numbers multiples of 5 return Buzz
+    //Numbers multiples of 3 and 5 return FizzBuzz
+
+
     @Test
-    void test_library(){
+    void inputNumbersNotMultiplesThreeOrFive() {
+        FizzBuzz fb = new FizzBuzz();
+        assertEquals(fb.play(1), "1");
+    }
+
+    @Test
+    void inputNumberMultiplesOfThree(){
+        FizzBuzz fb = new FizzBuzz();
+        assertEquals(fb.play(3), "Fizz");
+    }
+
+    @Test
+    void inputNumberMultiplesOfFive(){
+        FizzBuzz fb = new FizzBuzz();
+        assertEquals(fb.play(5), "Buzz");
+    }
+
+    @Test
+    void inputNumberMultiplesOfThreeAndFive(){
         FizzBuzz fb = new FizzBuzz();
         assertEquals(fb.play(15), "FizzBuzz");
     }
